b96959af9355125ddaa4a66bfd2e18a370a87a69
satwika1505 <satwikanoundru@gmail.com>
2025-08-31 09:58:31 +0530

    Initial commit - Weather

---
+++ components/footer.js
@@ -0,0 +1,5 @@
+document.write(`
+  <footer>
+    <p>SKILLFORGE E-LEARNING SOLUTIONS PRIVATE LIMITED</p>
+  </footer>
+`);
---
+++ components/header.js
@@ -0,0 +1,7 @@
+document.write(`
+  <header>
+    <div class="logo">SkillForge</div>
+    <h2>Web Development Project</h2>
+    <button id="toggle-theme">🌙 Toggle Theme</button>
+  </header>
+`);
